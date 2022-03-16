0c6739960ae183fcbd4c97bd3519052ad3b07aef
PaoloGeo <paolo.araujo@gmail.com>
2022-03-16 15:41:47 -0600

    Flight and Reservation classes created.

--- CPRG251A3Skeleton/src/sait/frms/problemdomain/Flight.java
+++ CPRG251A3Skeleton/src/sait/frms/problemdomain/Flight.java
@@ -1,5 +1,113 @@
 package sait.frms.problemdomain;
 
 public class Flight {
+	private String code;
+	private String airlineNameString;
+	private String from;
+	private String to;
+	private String weekday;
+	private String timeString;
+	private int seats;
+	private double costPerSeat;
+
+	public Flight() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Flight(String code, String airlineNameString, String from, String to, String weekday, String timeString,
+			int seats, double costPerSeat) {
+		super();
+		this.code = code;
+		this.airlineNameString = airlineNameString;
+		this.from = from;
+		this.to = to;
+		this.weekday = weekday;
+		this.timeString = timeString;
+		this.seats = seats;
+		this.costPerSeat = costPerSeat;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getAirlineNameString() {
+		return airlineNameString;
+	}
+
+	public void setAirlineNameString(String airlineNameString) {
+		this.airlineNameString = airlineNameString;
+	}
+
+	public String getFrom() {
+		return from;
+	}
+
+	public void setFrom(String from) {
+		this.from = from;
+	}
+
+	public String getTo() {
+		return to;
+	}
+
+	public void setTo(String to) {
+		this.to = to;
+	}
+
+	public String getWeekday() {
+		return weekday;
+	}
+
+	public void setWeekday(String weekday) {
+		this.weekday = weekday;
+	}
+
+	public String getTimeString() {
+		return timeString;
+	}
+
+	public void setTimeString(String timeString) {
+		this.timeString = timeString;
+	}
+
+	public int getSeats() {
+		return seats;
+	}
+
+	public void setSeats(int seats) {
+		this.seats = seats;
+	}
+
+	public double getCostPerSeat() {
+		return costPerSeat;
+	}
+
+	public void setCostPerSeat(double costPerSeat) {
+		this.costPerSeat = costPerSeat;
+	}
+
+	public boolean isDomestic() {
+		return false;
+	}
+	
+	public void parseCode(String code) {
+		
+	}
+
+	@Override
+	public String toString() {
+		return "Flight [code=" + code + ", airlineNameString=" + airlineNameString + ", from=" + from + ", to=" + to
+				+ ", weekday=" + weekday + ", timeString=" + timeString + ", seats=" + seats + ", costPerSeat="
+				+ costPerSeat + ", getCode()=" + getCode() + ", getAirlineNameString()=" + getAirlineNameString()
+				+ ", getFrom()=" + getFrom() + ", getTo()=" + getTo() + ", getWeekday()=" + getWeekday()
+				+ ", getTimeString()=" + getTimeString() + ", getSeats()=" + getSeats() + ", getCostPerSeat()="
+				+ getCostPerSeat() + "]";
+	}
 
 }
--- CPRG251A3Skeleton/src/sait/frms/problemdomain/Reservation.java
+++ CPRG251A3Skeleton/src/sait/frms/problemdomain/Reservation.java
@@ -1,5 +1,113 @@
 package sait.frms.problemdomain;
 
 public class Reservation {
+	private String code;
+	private String flightCode;
+	private String airline;
+	private String name;
+	private String citizenship;
+	private double cost;
+	private boolean active;
+	
+	
+	public Reservation() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Reservation(String code, String flightCode, String airline, String name, String citizenship, double cost,
+			boolean active) {
+		super();
+		this.code = code;
+		this.flightCode = flightCode;
+		this.airline = airline;
+		this.name = name;
+		this.citizenship = citizenship;
+		this.cost = cost;
+		this.active = active;
+	}
+
+
+	public String getCode() {
+		return code;
+	}
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
+	public String getFlightCode() {
+		return flightCode;
+	}
+
+
+	public void setFlightCode(String flightCode) {
+		this.flightCode = flightCode;
+	}
+
+
+	public String getAirline() {
+		return airline;
+	}
+
+
+	public void setAirline(String airline) {
+		this.airline = airline;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getCitizenship() {
+		return citizenship;
+	}
+
+
+	public void setCitizenship(String citizenship) {
+		this.citizenship = citizenship;
+	}
+
+
+	public double getCost() {
+		return cost;
+	}
+
+
+	public void setCost(double cost) {
+		this.cost = cost;
+	}
+
+
+	public boolean isActive() {
+		return active;
+	}
+
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Reservation [code=" + code + ", flightCode=" + flightCode + ", airline=" + airline + ", name=" + name
+				+ ", citizenship=" + citizenship + ", cost=" + cost + ", active=" + active + ", getCode()=" + getCode()
+				+ ", getFlightCode()=" + getFlightCode() + ", getAirline()=" + getAirline() + ", getName()=" + getName()
+				+ ", getCitizenship()=" + getCitizenship() + ", getCost()=" + getCost() + ", isActive()=" + isActive()
+				+ "]";
+	}
+	
+	
 
 }
